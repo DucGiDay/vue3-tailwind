cc36903dae0590d9cce4d08c0fea3e93053c743e
duc.ht <duc123zzmm@gmail.com>
2026-03-12 10:31:10 +0700

    cập nhật package json

--- src/views/pages/Dashboard.vue
+++ src/views/pages/Dashboard.vue
@@ -4,9 +4,13 @@ import NotificationsWidget from '@/components/PageComponent/dashboard/Notificati
 import RecentSalesWidget from '@/components/PageComponent/dashboard/RecentSalesWidget.vue';
 import RevenueStreamWidget from '@/components/PageComponent/dashboard/RevenueStreamWidget.vue';
 import StatsWidget from '@/components/PageComponent/dashboard/StatsWidget.vue';
+const mode = import.meta.env.MODE;
+const SUB_APP_URL = import.meta.env.VITE_SUB_APP_URL;
 </script>
 
 <template>
+  {{ mode }}
+  {{ SUB_APP_URL }}
   <div class="fb-grid fb-grid-cols-12 fb-gap-8">
     <StatsWidget />
 
