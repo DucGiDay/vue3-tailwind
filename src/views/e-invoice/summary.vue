4a11ea161c77402f3f084325073d36cbc2a2c440
Hoang Trung Duc <duc.hoang01@ipos.vn>
2026-06-23 10:11:24 +0700

    Giảm margin bottom title layout

--- src/views/e-invoice/summary.vue
+++ src/views/e-invoice/summary.vue
@@ -107,7 +107,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <div :class="['fb-flex fb-justify-between fb-items-center fb-mb-6']">
+  <div :class="['fb-flex fb-justify-between fb-items-center fb-mb-5']">
     <div class="fb-flex fb-items-center fb-space-x-3">
       <h5 class="!fb-m-0 !fb-text-lg !fb-font-semibold">Tổng quan</h5>
       <div 
